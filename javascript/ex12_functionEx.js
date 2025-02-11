8629215885a295b7b3017ebc30ca5e15d1a66470
yeonmi park <pyeonm25@gmail.com>
2025-02-11 15:33:46 +0900

    .
    .

---
+++ javascript/ex12_functionEx.js
@@ -0,0 +1,22 @@
+function printArray(start, end) { }
+
+
+printArray(1, 5);
+// for (let i = 1; i <= 5; i += 1) {
+//   console.log(i);
+// }
+// // 1 2 3 4 5 
+printArray(6, 15);
+// for (let i = 6; i <= 15; i += 1) {
+//   console.log(i);
+// }
+// // 6 7 8 9 10 11 12 13 14 15 
+
+printArray(15, 4);
+
+printArray('test', 4);
+// for (let i = 15; i >= 4; i -= 1) {
+//   console.log(i);
+// }
+
+// // 4 5 6 7 8 9 10 11 12 13 14 15 
