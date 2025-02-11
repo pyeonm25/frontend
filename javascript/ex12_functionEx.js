8282f44918f7f8ce4bffe8e0e07997ceb088cd40
yeonmi park <pyeonm25@gmail.com>
2025-02-11 16:04:24 +0900

    .
    .

--- javascript/ex12_functionEx.js
+++ javascript/ex12_functionEx.js
@@ -1,5 +1,22 @@
-function printArray(start, end) { }
+function printArray(start, end) {
+  let isValidNums = typeof start == 'number' && typeof end == 'number';
+  if (!isValidNums) {
+    console.log(" 숫자값을 입력하세요");
+    return;
+  }
 
+  if (start > end) {
+    let temp = start;
+    start = end;
+    end = temp;
+  }
+
+  let result = '';
+  for (let i = start; i <= end; i += 1)
+    result += i + " ";
+  console.log(result);
+}
+printArray();
 
 printArray(1, 5);
 // for (let i = 1; i <= 5; i += 1) {
@@ -15,6 +32,8 @@ printArray(6, 15);
 printArray(15, 4);
 
 printArray('test', 4);
+printArray(4, 'test');
+printArray('test', 'test');
 // for (let i = 15; i >= 4; i -= 1) {
 //   console.log(i);
 // }
