a4d82bf5a11d5c204d07fde914e6697cd865260d
yeonmi park <pyeonm25@gmail.com>
2025-02-11 17:37:00 +0900

    .
    .

--- javascript/ex15_arrayFn.js
+++ javascript/ex15_arrayFn.js
@@ -6,3 +6,46 @@ let array = [3, 5, 8, 6, 7, 11, 15, 14, 23];
 // foreach() => 배열요소 처음 끝까지 한개씩 넣어서 실행하는 함수
 
 // 문제 1 ) 짝수만 출력 
+// function printEvenNum(n) {
+//   if (n % 2 == 0) console.log(n);
+// }
+// array.forEach(printEvenNum);
+
+array.forEach(n => { if (n % 2 == 0) console.log(n) });
+
+// map() => 데이터를 받아서 새로운 값 가공 
+
+let result = array.map(x => x * 10);
+console.log(array);
+console.log(result);
+
+// filter : 조건식이 true 에만 실행 
+console.log("============")
+result = array.filter(val => val % 2 == 0);
+console.log(result);
+
+console.log("============")
+//find 
+result = array.find(val => val > 10);
+console.log(result);
+console.log("============")
+result = array.findIndex(val => val > 10);
+console.log(result);
+
+// reduce() => 줄이다..  : 배열 => 한개의 값 : 총합, 갯수 
+console.log("============")
+result = array.reduce((p, n) => {
+  console.log(`누적값 ${p} : 현재값 ${n}`);
+  return p + n;
+}, 0);
+console.log(result);
+console.log("============")
+// 홀수의 갯수를 리턴 
+result = array.reduce((cnt, val) => {
+  if (val % 2 == 0) return cnt;
+  else {
+    console.log("val=" + val + "cnt=" + cnt)
+    return cnt += 1;
+  }
+}, 0)
+console.log("홀수의 갯수  = " + result);
