812d32357e6898c4a79eff91bfb47cb3f82f696f
yeonmi park <pyeonm25@gmail.com>
2025-02-12 15:01:42 +0900

    .
    .

---
+++ javascript/ex27_context2.js
@@ -0,0 +1,12 @@
+let num1 = 10;
+let num2 = 20;
+
+function add(x, y) {
+  let result = x + y;
+  return result;
+}
+
+let mulply = (x, y) => x * y;
+
+let res1 = add(num1, num2);
+let res2 = mulply(num1, num2);
