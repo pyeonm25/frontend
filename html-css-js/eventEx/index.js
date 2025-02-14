ee784e48dec000072d3e11b33e64a9c9cc3ca267
yeonmi park <pyeonm25@gmail.com>
2025-02-14 10:00:59 +0900

    .
    .

---
+++ html-css-js/eventEx/index.js
@@ -0,0 +1,4 @@
+const vartical = document.querySelector('.vartical');
+const horizontal = document.querySelector('.horizontal');
+const img = document.querySelector('img');
+const span = document.querySelector('span');
